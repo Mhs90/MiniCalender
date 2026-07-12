692268a84daa97b37034ead222ef5f5f748fc833
Mohammad Hossein Sanavi <sanavi.mohammad90@gmail.ir>
2026-07-12 16:25:50 +0330

    LastUpdate

--- script.js
+++ script.js
@@ -18,73 +18,77 @@ const solarMonth = new Intl.DateTimeFormat('fa-IR-u-nu-latn', {
 const solarDay = new Intl.DateTimeFormat('fa-IR-u-nu-arabext', {
     day: 'numeric',
 }).format(new Date());
-//month
-switch (Month) {
-    case 0:
-        monthEl.textContent = "January"
-        break;
-    case 1:
-        monthEl.textContent = "February"
-        break;
-    case 2:
-        monthEl.textContent = "March"
-        break;
-    case 3:
-        monthEl.textContent = "April"
-        break;
-    case 4:
-        monthEl.textContent = "May"
-        break;
-    case 5:
-        monthEl.textContent = "June"
-        break;
-    case 6:
-        monthEl.textContent = "July"
-        break;
-    case 7:
-        monthEl.textContent = "August"
-        break;
-    case 8:
-        monthEl.textContent = "September"
-        break;
-    case 9:
-        monthEl.textContent = "October"
-        break;
-    case 10:
-        monthEl.textContent = "November"
-        break;
-    case 11:
-        monthEl.textContent = "December"
-        break;
-    default:
-        break;
+function monthSwitch() {
+    switch (Month) {
+        case 0:
+            monthEl.textContent = "January"
+            break;
+        case 1:
+            monthEl.textContent = "February"
+            break;
+        case 2:
+            monthEl.textContent = "March"
+            break;
+        case 3:
+            monthEl.textContent = "April"
+            break;
+        case 4:
+            monthEl.textContent = "May"
+            break;
+        case 5:
+            monthEl.textContent = "June"
+            break;
+        case 6:
+            monthEl.textContent = "July"
+            break;
+        case 7:
+            monthEl.textContent = "August"
+            break;
+        case 8:
+            monthEl.textContent = "September"
+            break;
+        case 9:
+            monthEl.textContent = "October"
+            break;
+        case 10:
+            monthEl.textContent = "November"
+            break;
+        case 11:
+            monthEl.textContent = "December"
+            break;
+        default:
+            break;
+    }
 }
-//whichDay
-switch (WhichDay) {
-    case 0:
-        whichDayEl.textContent = "Sunday"
-        break;
-    case 1:
-        whichDayEl.textContent = "Monday"
-        break;
-    case 2:
-        whichDayEl.textContent = "Tuesday"
-        break;
-    case 3:
-        whichDayEl.textContent = "Wednesday"
-        break;
-    case 4:
-        whichDayEl.textContent = "Thursday"
-        break;
-    case 5:
-        whichDayEl.textContent = "Friday"
-        break;
-    case 6:
-        whichDayEl.textContent = "Saturday"
-        break;
-    default:
-        break;
+function whichDaySwitch() {
+    switch (WhichDay) {
+        case 0:
+            whichDayEl.textContent = "Sunday"
+            break;
+        case 1:
+            whichDayEl.textContent = "Monday"
+            break;
+        case 2:
+            whichDayEl.textContent = "Tuesday"
+            break;
+        case 3:
+            whichDayEl.textContent = "Wednesday"
+            break;
+        case 4:
+            whichDayEl.textContent = "Thursday"
+            break;
+        case 5:
+            whichDayEl.textContent = "Friday"
+            break;
+        case 6:
+            whichDayEl.textContent = "Saturday"
+            break;
+        default:
+            break;
+    }
 }
+monthSwitch();
+whichDaySwitch();
 dayEl.textContent = Day;
 yearEl.textContent = Number(Year) + 1900;
 
@@ -163,72 +167,8 @@ const btnClickHandler = () => {
         changeBtnEl.textContent = "calendar"
         isSolar = true;
     } else {
-        switch (Month) {
-            case 0:
-                monthEl.textContent = "January"
-                break;
-            case 1:
-                monthEl.textContent = "February"
-                break;
-            case 2:
-                monthEl.textContent = "March"
-                break;
-            case 3:
-                monthEl.textContent = "April"
-                break;
-            case 4:
-                monthEl.textContent = "May"
-                break;
-            case 5:
-                monthEl.textContent = "June"
-                break;
-            case 6:
-                monthEl.textContent = "July"
-                break;
-            case 7:
-                monthEl.textContent = "August"
-                break;
-            case 8:
-                monthEl.textContent = "September"
-                break;
-            case 9:
-                monthEl.textContent = "October"
-                break;
-            case 10:
-                monthEl.textContent = "November"
-                break;
-            case 11:
-                monthEl.textContent = "December"
-                break;
-            default:
-                break;
-        }
-        //whichDay
-        switch (WhichDay) {
-            case 0:
-                whichDayEl.textContent = "Sunday"
-                break;
-            case 1:
-                whichDayEl.textContent = "Monday"
-                break;
-            case 2:
-                whichDayEl.textContent = "Tuesday"
-                break;
-            case 3:
-                whichDayEl.textContent = "Wednesday"
-                break;
-            case 4:
-                whichDayEl.textContent = "Thursday"
-                break;
-            case 5:
-                whichDayEl.textContent = "Friday"
-                break;
-            case 6:
-                whichDayEl.textContent = "Saturday"
-                break;
-            default:
-                break;
-        }
+        monthSwitch()
+        whichDaySwitch()
         dayEl.textContent = Day;
         yearEl.textContent = Number(Year) + 1900;
         changeBtnEl.style.backgroundColor = "#bab9cf";
